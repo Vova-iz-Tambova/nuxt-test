463f79270fa8e7784ae5ed647ea9e35dcf38642d
Vova iz Tambova <voller.ruden@yandex.ru>
2024-02-16 23:08:54 +0300

    add tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+
+    <template>
+      <nav class="flex align-center gap-4 p4">
+        <NuxtLink to="/" class="n-linj-base"> Home </NuxtLink>
+        <NuxtLink to="/about" class="n-linj-base"> About </NuxtLink>
+      </nav>
+    </template>
+    <NuxtLoadingIndicator />
+		<NuxtPage />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import { useFetch } from 'nuxt/app'
+
+const { data } = await useFetch('http://localhost:3002/posts')
+</script>
+
+<template>
+  <div v-if="isLoading">Loading...</div>
+  <div v-else-if="!data.length">Posts not found</div>
+
+  <ul v-else>
+    <li :key="post.id" v-for="post in data">
+      <span>{{ post.id }} - {{ post.title }}</span>
+      <!-- <button :disabled="isLoadingDelete" @click="mutate(post.id)">X</button> -->
+    </li>
+  </ul>
+</template>
